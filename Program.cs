c26aaee26f71d9b4ee0c880c191e63f2a8081c16
StepanErin <erinstepanvit@gmail.com>
2023-05-11 19:48:08 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGit_11._05._23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
